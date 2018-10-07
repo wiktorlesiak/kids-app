e253ea347a3747529cf40c1d0d802721257a0f55
Wiktor Lesiak <B00087701@student.itb.ie>
2018-10-07 21:04:54 +0100

    initial app build

---
+++ KidsApp/app/src/main/java/com/lesiak/wiktor/kidsapp/kids_app.java
@@ -0,0 +1,13 @@
+package com.lesiak.wiktor.kidsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class kids_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kids_app);
+    }
+}
